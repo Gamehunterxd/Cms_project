3e6e6a7c0d3a0bf253a9172692107e5605cc2334
Gamehunterxd <Gamehunterxd@hotmail.dk>
2019-10-08 22:57:06 +0200

    Adding in navigation, shop-setup, googlechart functions and dashboard setup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-let a = 5;
-let b = 6;
-let c = a+b;
-let d = c+a;
-let e = a+b-d+c;
-console.log(e);
+$(document).ready(function () {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['måned', 'profit', 'Expenses'],
+            ['August',  65842,      32541],
+            ['Semptember',  64567,      33145],
+            ['November',  55214,       33254],
+            ['December',  77512,      34251]
+        ]);
+
+        var options = {
+            title: 'Company Performance',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('differential'));
+
+        chart.draw(data, options);
+    }
+});
