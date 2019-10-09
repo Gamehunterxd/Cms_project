5b557c2a674d555c32f351743644f18931d25c7f
Gamehunterxd <Gamehunterxd@hotmail.dk>
2019-10-09 03:08:13 +0200

    Working dashboard

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
     google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {'packages':['bar']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
             ['måned', 'profit', 'Expenses'],
             ['August',  65842,      32541],
-            ['Semptember',  64567,      33145],
-            ['November',  55214,       33254],
-            ['December',  77512,      34251]
+            ['September',  64567,      33145],
+            ['Oktober',  55214,       33254],
+            ['November',  77512,      34251]
         ]);
 
         var options = {
-            title: 'Company Performance',
+            title: 'Indkomst og udgifter',
             curveType: 'function',
             legend: { position: 'bottom' }
         };
@@ -20,5 +21,32 @@ $(document).ready(function () {
         var chart = new google.visualization.LineChart(document.getElementById('differential'));
 
         chart.draw(data, options);
+    }
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawStuff);
+
+    function drawStuff() {
+        var data = new google.visualization.arrayToDataTable([
+            ['måned', 'salg'],
+            ["August", 21523],
+            ["September", 21643],
+            ["Oktober", 19251],
+            ["November", 23125]
+            ]);
+
+        var options = {
+            chart: {
+                title: 'Salg',
+                subtitle: 'fra august til november' },
+            axes: {
+                x: {
+                    0: { side: 'top', label: 'salg'}
+                }
+            },
+            bar: { groupWidth: "100%" }
+        };
+        var chart = new google.charts.Bar(document.getElementById('sales'));
+        // Convert the Classic options to Material options.
+        chart.draw(data, google.charts.Bar.convertOptions(options));
     }
 });
